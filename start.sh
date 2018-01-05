b1fe0f03a059d381f2e0ac8fe9cb214df44d83c4
Jeff Vogt <jvogt@chef.io>
2018-01-04 17:24:38 -0800

    first

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Starting docker compose project"
+docker-compose up -d
+
+echo "Cloning Sample App repo"
+ssh-keygen -R [localhost]:2222
+git clone ssh://git@localhost:2222/git-server/repos/rallyjenkinsdemo-sampleapp.git sampleapp
+
+echo "Waiting for Jenkins to start..."
+sleep 10
+
+echo "Opening Browsers"
+open *.webloc
+
